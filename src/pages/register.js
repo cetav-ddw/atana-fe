df116ce140b9c41de061c364276f4e70fb798310
unknown <cristianpipe0494@gmail.com>
2019-10-31 09:06:37 -0600

    add: npm package to communicate with mailchimp and created new page for the register section

---
+++ src/pages/register.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+class Register extends Component {
+  constructor() {
+    this.state = {
+    }
+  }
+  render () {
+    return(
+      <form>
+      </form>
+    )
+  }
+}
+
+export default Register;
