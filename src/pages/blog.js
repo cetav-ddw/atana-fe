03af565b15629c994770226e58aa85035c02b9e9
unknown <cristianpipe0494@gmail.com>
2019-12-05 17:09:54 -0600

    fixed: render post functionality

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,74 +1,78 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
-import SEO from '../components/seo';
-import Menu from '../components/sections/nav-menu';
-import FooterContact from '../components/sections/footer-contact';
+import React from "react"
+import { Link, graphql, StaticQuery } from "gatsby"
+import SEO from "../components/seo"
+import Menu from "../components/sections/nav-menu"
+import FooterContact from "../components/sections/footer-contact"
+import "../styles/blog.scss"
 
-import '../styles/blog.scss';
+const BlogIndex = () => (
+  <StaticQuery
+    query={
+      graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+          allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+            edges {
+              node {
+                excerpt
+                fields {
+                  slug
+                }
+                frontmatter {
+                  date(formatString: "MMMM DD, YYYY")
+                  title
+                  description
+                }
+              }
+            }
+          }
+        }
+      `}
 
-class BlogPage extends React.Component {
-  render() {
-    const { data } = this.props;
-    const posts = data.allMarkdownRemark.edges;
+      render={(data) => {
+        const posts = data.allMarkdownRemark.edges
 
-    return posts.map(({ node }) => {
-      // ToDo porque el OR? el slug es algo como mi-post, como eso
-      // va a ser el title?
-      const title = node.frontmatter.title || node.fields.slug;
-
+        const post = posts.map(({ node }) => {
+          const title = node.frontmatter.title
+          const path = node.fields.slug.replace(/\/$/gm, "")
+          return (
+            <div className="section__wrapper">
+              <div key={node.fields.slug} className="post__wrapper">
+                <Link to={path} className="post">
+                  <h4 className="post__title">{title}</h4>
+                  <small className="post__date post__preview">
+                    {node.frontmatter.date}
+                  </small>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                    className="post__preview"
+                  />
+                </Link>
+              </div>
+            </div>
+          )
+      })
       return (
-        <>
+        <React.Fragment>
           <SEO title="Blog" />
           <div className="menu">
             <Menu />
           </div>
           <div className="section__wrapper">
             <h2 className="blog__title">Átana blog</h2>
-            <div key={node.fields.slug} className="post__wrapper">
-              <Link to={node.fields.slug} className="post">
-                <h4 className="post__title">{title}</h4>
-                <small className="post__date post__preview">
-                  {node.frontmatter.date}
-                </small>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                  className="post__preview"
-                />
-              </Link>
-            </div>
           </div>
+          {post}
           <FooterContact />
-        </>
-      );
-    });
-  }
-}
+        </React.Fragment>
+      )
+    }}
+  ></StaticQuery>
+)
 
-export default BlogPage;
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-`;
+export default BlogIndex
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
+import SEO from "../components/seo"
+import Menu from "../components/sections/nav-menu"
+import FooterContact from "../components/sections/footer-contact"
+import "../styles/blog.scss"
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.markdownRemark;
-    const siteTitle = this.props.data.site.siteMetadata.title;
-    const { previous, next } = this.props.pageContext;
+    const post = this.props.data.markdownRemark
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
 
     return (
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <p>{post.frontmatter.date}</p>
-        <p dangerouslySetInnerHTML={{ __html: post.html }} />
+      <React.Fragment>
+      <SEO title="Home" />
+      <div className="menu">
+        <Menu />
       </div>
-    );
+      <div className="section__wrapper">
+        <div className="post__wrapper">
+          <h1 className="post__title">{post.frontmatter.title}</h1>
+          <p className="post__date post__preview">{post.frontmatter.date}</p>
+          <p dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
+      </div>
+      <FooterContact />
+    </React.Fragment>
+    )
   }
 }
 
-export default BlogPostTemplate;
+export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -37,4 +50,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
