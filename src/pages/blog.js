8c4c66a762c53a5f3609ea70331dd180a108d376
unknown <cristianpipe0494@gmail.com>
2019-12-05 17:28:13 -0600

    format code

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,63 +1,61 @@
-import React from "react"
-import { Link, graphql, StaticQuery } from "gatsby"
-import SEO from "../components/seo"
-import Menu from "../components/sections/nav-menu"
-import FooterContact from "../components/sections/footer-contact"
-import "../styles/blog.scss"
+import React from 'react';
+import { Link, graphql, StaticQuery } from 'gatsby';
+import SEO from '../components/seo';
+import Menu from '../components/sections/nav-menu';
+import FooterContact from '../components/sections/footer-contact';
+import '../styles/blog.scss';
 
 const BlogIndex = () => (
   <StaticQuery
-    query={
-      graphql`
-        query {
-          site {
-            siteMetadata {
-              title
-            }
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
           }
-          allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-            edges {
-              node {
-                excerpt
-                fields {
-                  slug
-                }
-                frontmatter {
-                  date(formatString: "MMMM DD, YYYY")
-                  title
-                  description
-                }
+        }
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+          edges {
+            node {
+              excerpt
+              fields {
+                slug
+              }
+              frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                title
+                description
               }
             }
           }
         }
-      `}
+      }
+    `}
+    render={data => {
+      const posts = data.allMarkdownRemark.edges;
 
-      render={(data) => {
-        const posts = data.allMarkdownRemark.edges
-
-        const post = posts.map(({ node }) => {
-          const title = node.frontmatter.title
-          const path = node.fields.slug.replace(/\/$/gm, "")
-          return (
-            <div className="section__wrapper">
-              <div key={node.fields.slug} className="post__wrapper">
-                <Link to={path} className="post">
-                  <h4 className="post__title">{title}</h4>
-                  <small className="post__date post__preview">
-                    {node.frontmatter.date}
-                  </small>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                    className="post__preview"
-                  />
-                </Link>
-              </div>
+      const post = posts.map(({ node }) => {
+        const title = node.frontmatter.title;
+        const path = node.fields.slug.replace(/\/$/gm, '');
+        return (
+          <div className="section__wrapper">
+            <div key={node.fields.slug} className="post__wrapper">
+              <Link to={path} className="post">
+                <h4 className="post__title">{title}</h4>
+                <small className="post__date post__preview">
+                  {node.frontmatter.date}
+                </small>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                  className="post__preview"
+                />
+              </Link>
             </div>
-          )
-      })
+          </div>
+        );
+      });
       return (
         <React.Fragment>
           <SEO title="Blog" />
@@ -70,9 +68,9 @@ const BlogIndex = () => (
           {post}
           <FooterContact />
         </React.Fragment>
-      )
+      );
     }}
   ></StaticQuery>
-)
+);
 
-export default BlogIndex
+export default BlogIndex;
